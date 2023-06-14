aac15bdd5c58393d78f8ee4a41d30af926b892c4
Tejesh Kumar S <zenc0derr>
2023-06-14 23:21:26 +0530

    set schedule start_time and end_time

--- backend/controller/admin/admin.controller.js
+++ backend/controller/admin/admin.controller.js
@@ -3,6 +3,7 @@ const adminDAO = require("../../dao/admin/admin.dao")
 class adminController{
     static async apiSetSchedule(req, res){
         try{
+            console.log(req.body.start_time)
             const posted = await adminDAO.setSchedule(req.body)
             res.status(200).json({status: "success"})
         }catch(e){
